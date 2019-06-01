970cf572cce2121feaa7c2e24990efd4fb090281
romanshen <siangching.shen@gmail.com>
2019-06-01 16:04:11 +0800

    finished image processing

--- dataloader.py
+++ dataloader.py
@@ -14,6 +14,14 @@ import torch.utils.data as data
 
 import multiprocessing
 import six
+from torchvision import transforms as trn
+
+
+preprocess = trn.Compose([
+    # trn.ToTensor(),
+    trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
 
 class HybridLoader:
     """
@@ -21,6 +29,7 @@ class HybridLoader:
     If lmdb, then load from lmdb
     The loading method depend on extention.
     """
+
     def __init__(self, db_path, ext):
         self.db_path = db_path
         self.ext = ext
@@ -31,11 +40,11 @@ class HybridLoader:
         if db_path.endswith('.lmdb'):
             self.db_type = 'lmdb'
             self.env = lmdb.open(db_path, subdir=os.path.isdir(db_path),
-                                readonly=True, lock=False,
-                                readahead=False, meminit=False)
+                                 readonly=True, lock=False,
+                                 readahead=False, meminit=False)
         else:
             self.db_type = 'dir'
-    
+
     def get(self, key):
 
         if self.db_type == 'lmdb':
@@ -56,7 +65,8 @@ class DataLoader(data.Dataset):
 
     def reset_iterator(self, split):
         del self._prefetch_process[split]
-        self._prefetch_process[split] = BlobFetcher(split, self, split=='train')
+        self._prefetch_process[split] = BlobFetcher(
+            split, self, split == 'train')
         self.iterators[split] = 0
 
     def get_vocab_size(self):
@@ -72,7 +82,7 @@ class DataLoader(data.Dataset):
         self.opt = opt
         self.batch_size = self.opt.batch_size
         self.seq_per_img = opt.seq_per_img
-        
+
         # feature related options
         self.use_fc = getattr(opt, 'use_fc', True)
         self.use_att = getattr(opt, 'use_att', True)
@@ -86,14 +96,28 @@ class DataLoader(data.Dataset):
         self.ix_to_word = self.info['ix_to_word']
         self.vocab_size = len(self.ix_to_word)
         print('vocab size is ', self.vocab_size)
-        
+
         # open the hdf5 file
-        print('DataLoader loading h5 file: ', opt.input_fc_dir, opt.input_att_dir, opt.input_box_dir, opt.input_label_h5)
-        self.h5_label_file = h5py.File(self.opt.input_label_h5, 'r', driver='core')
+        print('DataLoader loading h5 file: ', opt.input_fc_dir, opt.input_att_dir,
+              opt.input_box_dir, opt.input_label_h5, opt.input_image_h5)
+        self.h5_label_file = h5py.File(
+            self.opt.input_label_h5, 'r', driver='core')
+        self.h5_image_file = h5py.File(
+            self.opt.input_image_h5, 'r', driver='core')
 
         self.fc_loader = HybridLoader(self.opt.input_fc_dir, '.npy')
         self.att_loader = HybridLoader(self.opt.input_att_dir, '.npz')
         self.box_loader = HybridLoader(self.opt.input_box_dir, '.npy')
+
+        # extract image size from dataset
+        images_size = self.h5_image_file['images'].shape
+        assert len(images_size) == 4, 'images should be a 4D tensor'
+        assert images_size[2] == images_size[3], 'width and height must match'
+        self.num_images = images_size[0]
+        self.num_channels = images_size[1]
+        self.max_image_size = images_size[2]
+        print('read %d images of size %dx%dx%d' % (self.num_images,
+                                                   self.num_channels, self.max_image_size, self.max_image_size))
 
         # load in the sequence data
         seq_size = self.h5_label_file['labels'].shape
@@ -104,7 +128,7 @@ class DataLoader(data.Dataset):
         self.label_end_ix = self.h5_label_file['label_end_ix'][:]
 
         self.num_images = self.label_start_ix.shape[0]
-        print('read %d image features' %(self.num_images))
+        print('read %d image features' % (self.num_images))
 
         # separate out indexes for each of the provided splits
         self.split_ix = {'train': [], 'val': [], 'test': []}
@@ -116,19 +140,22 @@ class DataLoader(data.Dataset):
                 self.split_ix['val'].append(ix)
             elif img['split'] == 'test':
                 self.split_ix['test'].append(ix)
-            elif opt.train_only == 0: # restval
+            elif opt.train_only == 0:  # restval
                 self.split_ix['train'].append(ix)
 
-        print('assigned %d images to split train' %len(self.split_ix['train']))
-        print('assigned %d images to split val' %len(self.split_ix['val']))
-        print('assigned %d images to split test' %len(self.split_ix['test']))
+        print('assigned %d images to split train' %
+              len(self.split_ix['train']))
+        print('assigned %d images to split val' % len(self.split_ix['val']))
+        print('assigned %d images to split test' % len(self.split_ix['test']))
 
         self.iterators = {'train': 0, 'val': 0, 'test': 0}
-        
-        self._prefetch_process = {} # The three prefetch process
+
+        self._prefetch_process = {}  # The three prefetch process
         for split in self.iterators.keys():
-            self._prefetch_process[split] = BlobFetcher(split, self, split=='train')
+            self._prefetch_process[split] = BlobFetcher(
+                split, self, split == 'train')
             # Terminate the child process when the parent exists
+
         def cleanup():
             print('Terminating BlobFetcher')
             for split in self.iterators.keys():
@@ -138,20 +165,21 @@ class DataLoader(data.Dataset):
 
     def get_captions(self, ix, seq_per_img):
         # fetch the sequence labels
-        ix1 = self.label_start_ix[ix] - 1 #label_start_ix starts from 1
+        ix1 = self.label_start_ix[ix] - 1  # label_start_ix starts from 1
         ix2 = self.label_end_ix[ix] - 1
-        ncap = ix2 - ix1 + 1 # number of captions available for this image
+        ncap = ix2 - ix1 + 1  # number of captions available for this image
         assert ncap > 0, 'an image does not have any label. this can be handled but right now isn\'t'
 
         if ncap < seq_per_img:
             # we need to subsample (with replacement)
-            seq = np.zeros([seq_per_img, self.seq_length], dtype = 'int')
+            seq = np.zeros([seq_per_img, self.seq_length], dtype='int')
             for q in range(seq_per_img):
-                ixl = random.randint(ix1,ix2)
+                ixl = random.randint(ix1, ix2)
                 seq[q, :] = self.h5_label_file['labels'][ixl, :self.seq_length]
         else:
             ixl = random.randint(ix1, ix2 - seq_per_img + 1)
-            seq = self.h5_label_file['labels'][ixl: ixl + seq_per_img, :self.seq_length]
+            seq = self.h5_label_file['labels'][ixl: ixl
+                                               + seq_per_img, :self.seq_length]
 
         return seq
 
@@ -159,9 +187,13 @@ class DataLoader(data.Dataset):
         batch_size = batch_size or self.batch_size
         seq_per_img = seq_per_img or self.seq_per_img
 
-        fc_batch = [] # np.ndarray((batch_size * seq_per_img, self.opt.fc_feat_size), dtype = 'float32')
-        att_batch = [] # np.ndarray((batch_size * seq_per_img, 14, 14, self.opt.att_feat_size), dtype = 'float32')
-        label_batch = [] #np.zeros([batch_size * seq_per_img, self.seq_length + 2], dtype = 'int')
+        # np.ndarray((batch_size * seq_per_img, self.opt.fc_feat_size), dtype = 'float32')
+        fc_batch = []
+        # np.ndarray((batch_size * seq_per_img, 14, 14, self.opt.att_feat_size), dtype = 'float32')
+        att_batch = []
+        # np.zeros([batch_size * seq_per_img, self.seq_length + 2], dtype = 'int')
+        label_batch = []
+        img_batch = np.ndarray([batch_size, 3, 256, 256], dtype='float32')
 
         wrapped = False
 
@@ -177,14 +209,23 @@ class DataLoader(data.Dataset):
 
             fc_batch.append(tmp_fc)
             att_batch.append(tmp_att)
-            
-            tmp_label = np.zeros([seq_per_img, self.seq_length + 2], dtype = 'int')
-            tmp_label[:, 1 : self.seq_length + 1] = self.get_captions(ix, seq_per_img)
+
+            tmp_label = np.zeros(
+                [seq_per_img, self.seq_length + 2], dtype='int')
+            tmp_label[:, 1: self.seq_length +
+                      1] = self.get_captions(ix, seq_per_img)
             label_batch.append(tmp_label)
 
             # Used for reward evaluation
-            gts.append(self.h5_label_file['labels'][self.label_start_ix[ix] - 1: self.label_end_ix[ix]])
-        
+            gts.append(
+                self.h5_label_file['labels'][self.label_start_ix[ix] - 1: self.label_end_ix[ix]])
+
+            # fetch image
+            # img = self.load_image(self.image_info[ix]['filename'])
+            img = self.h5_image_file['images'][ix, :, :, :]
+            img_batch[i] = preprocess(torch.from_numpy(
+                img.astype('float32') / 255.0)).numpy()
+
             # record associated info as well
             info_dict = {}
             info_dict['ix'] = ix
@@ -196,34 +237,46 @@ class DataLoader(data.Dataset):
         # fc_batch, att_batch, label_batch, gts, infos = \
         #     zip(*sorted(zip(fc_batch, att_batch, np.vsplit(label_batch, batch_size), gts, infos), key=lambda x: len(x[1]), reverse=True))
         fc_batch, att_batch, label_batch, gts, infos = \
-            zip(*sorted(zip(fc_batch, att_batch, label_batch, gts, infos), key=lambda x: 0, reverse=True))
+            zip(*sorted(zip(fc_batch, att_batch, label_batch,
+                            gts, infos), key=lambda x: 0, reverse=True))
         data = {}
-        data['fc_feats'] = np.stack(sum([[_]*seq_per_img for _ in fc_batch], []))
+        data['fc_feats'] = np.stack(
+            sum([[_] * seq_per_img for _ in fc_batch], []))
         # merge att_feats
         max_att_len = max([_.shape[0] for _ in att_batch])
-        data['att_feats'] = np.zeros([len(att_batch)*seq_per_img, max_att_len, att_batch[0].shape[1]], dtype = 'float32')
+        data['att_feats'] = np.zeros(
+            [len(att_batch) * seq_per_img, max_att_len, att_batch[0].shape[1]], dtype='float32')
         for i in range(len(att_batch)):
-            data['att_feats'][i*seq_per_img:(i+1)*seq_per_img, :att_batch[i].shape[0]] = att_batch[i]
-        data['att_masks'] = np.zeros(data['att_feats'].shape[:2], dtype='float32')
+            data['att_feats'][i
+                              * seq_per_img:(i + 1) * seq_per_img, :att_batch[i].shape[0]] = att_batch[i]
+        data['att_masks'] = np.zeros(
+            data['att_feats'].shape[:2], dtype='float32')
         for i in range(len(att_batch)):
-            data['att_masks'][i*seq_per_img:(i+1)*seq_per_img, :att_batch[i].shape[0]] = 1
+            data['att_masks'][i
+                              * seq_per_img:(i + 1) * seq_per_img, :att_batch[i].shape[0]] = 1
         # set att_masks to None if attention features have same length
         if data['att_masks'].sum() == data['att_masks'].size:
             data['att_masks'] = None
 
         data['labels'] = np.vstack(label_batch)
+        data['images'] = img_batch
         # generate mask
-        nonzeros = np.array(list(map(lambda x: (x != 0).sum()+2, data['labels'])))
-        mask_batch = np.zeros([data['labels'].shape[0], self.seq_length + 2], dtype = 'float32')
+        nonzeros = np.array(
+            list(map(lambda x: (x != 0).sum() + 2, data['labels'])))
+        mask_batch = np.zeros(
+            [data['labels'].shape[0], self.seq_length + 2], dtype='float32')
         for ix, row in enumerate(mask_batch):
             row[:nonzeros[ix]] = 1
         data['masks'] = mask_batch
 
-        data['gts'] = gts # all ground truth captions of each images
-        data['bounds'] = {'it_pos_now': self.iterators[split], 'it_max': len(self.split_ix[split]), 'wrapped': wrapped}
+        data['gts'] = gts  # all ground truth captions of each images
+        data['bounds'] = {'it_pos_now': self.iterators[split],
+                          'it_max': len(self.split_ix[split]), 'wrapped': wrapped}
         data['infos'] = infos
 
-        data = {k:torch.from_numpy(v) if type(v) is np.ndarray else v for k,v in data.items()} # Turn all ndarray to torch tensor
+        # Turn all ndarray to torch tensor
+        data = {k: torch.from_numpy(v) if type(
+            v) is np.ndarray else v for k, v in data.items()}
 
         return data
 
@@ -233,26 +286,32 @@ class DataLoader(data.Dataset):
     def __getitem__(self, index):
         """This function returns a tuple that is further passed to collate_fn
         """
-        ix = index #self.split_ix[index]
+        ix = index  # self.split_ix[index]
         if self.use_att:
             att_feat = self.att_loader.get(str(self.info['images'][ix]['id']))
             # Reshape to K x C
             att_feat = att_feat.reshape(-1, att_feat.shape[-1])
             if self.norm_att_feat:
-                att_feat = att_feat / np.linalg.norm(att_feat, 2, 1, keepdims=True)
+                att_feat = att_feat / \
+                    np.linalg.norm(att_feat, 2, 1, keepdims=True)
             if self.use_box:
-                box_feat = self.box_loader.get(str(self.info['images'][ix]['id']))
+                box_feat = self.box_loader.get(
+                    str(self.info['images'][ix]['id']))
                 # devided by image width and height
-                x1,y1,x2,y2 = np.hsplit(box_feat, 4)
-                h,w = self.info['images'][ix]['height'], self.info['images'][ix]['width']
-                box_feat = np.hstack((x1/w, y1/h, x2/w, y2/h, (x2-x1)*(y2-y1)/(w*h))) # question? x2-x1+1??
+                x1, y1, x2, y2 = np.hsplit(box_feat, 4)
+                h, w = self.info['images'][ix]['height'], self.info['images'][ix]['width']
+                # question? x2-x1+1??
+                box_feat = np.hstack(
+                    (x1 / w, y1 / h, x2 / w, y2 / h, (x2 - x1) * (y2 - y1) / (w * h)))
                 if self.norm_box_feat:
-                    box_feat = box_feat / np.linalg.norm(box_feat, 2, 1, keepdims=True)
+                    box_feat = box_feat / \
+                        np.linalg.norm(box_feat, 2, 1, keepdims=True)
                 att_feat = np.hstack([att_feat, box_feat])
                 # sort the features by the size of boxes
-                att_feat = np.stack(sorted(att_feat, key=lambda x:x[-1], reverse=True))
+                att_feat = np.stack(
+                    sorted(att_feat, key=lambda x: x[-1], reverse=True))
         else:
-            att_feat = np.zeros((1,1,1), dtype='float32')
+            att_feat = np.zeros((1, 1, 1), dtype='float32')
         if self.use_fc:
             fc_feat = self.fc_loader.get(str(self.info['images'][ix]['id']))
         else:
@@ -263,6 +322,7 @@ class DataLoader(data.Dataset):
 
     def __len__(self):
         return len(self.info['images'])
+
 
 class SubsetSampler(torch.utils.data.sampler.Sampler):
     r"""Samples elements randomly from a given list of indices, without replacement.
@@ -279,8 +339,10 @@ class SubsetSampler(torch.utils.data.sampler.Sampler):
     def __len__(self):
         return len(self.indices)
 
+
 class BlobFetcher():
     """Experimental class for prefetching blobs in a separate process."""
+
     def __init__(self, split, dataloader, if_shuffle=False):
         """
         db is a list of tuples containing: imcrop_name, caption, bbox_feat of gt box, imname
@@ -298,12 +360,13 @@ class BlobFetcher():
         """
         # batch_size is 1, the merge is done in DataLoader class
         self.split_loader = iter(data.DataLoader(dataset=self.dataloader,
-                                            batch_size=1,
-                                            sampler=SubsetSampler(self.dataloader.split_ix[self.split][self.dataloader.iterators[self.split]:]),
-                                            shuffle=False,
-                                            pin_memory=True,
-                                            num_workers=4, # 4 is usually enough
-                                            collate_fn=lambda x: x[0]))
+                                                 batch_size=1,
+                                                 sampler=SubsetSampler(
+                                                     self.dataloader.split_ix[self.split][self.dataloader.iterators[self.split]:]),
+                                                 shuffle=False,
+                                                 pin_memory=True,
+                                                 num_workers=4,  # 4 is usually enough
+                                                 collate_fn=lambda x: x[0]))
 
     def _get_next_minibatch_inds(self):
         max_index = len(self.dataloader.split_ix[self.split])
@@ -321,7 +384,7 @@ class BlobFetcher():
         self.dataloader.iterators[self.split] = ri_next
 
         return ix, wrapped
-    
+
     def get(self):
         if not hasattr(self, 'split_loader'):
             self.reset()
